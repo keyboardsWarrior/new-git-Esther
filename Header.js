877aca74d372697e64756c3617d475b6a148e4ae
Esther <estersolo2584@gmail.com>
2022-09-13 22:38:03 +0300

    it's the update between 9-21 lessons

---
+++ Header.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+export default function Header(){
+
+    return (
+<header>
+<img src="./react-logo.png" width="40px" />
+<h1>Fun facts about React</h1>
+<UseUl/>
+</header>
+
+    )
+}    
